ade3bf917e4b77a1f85ab84dac9c8eb6ac695d12
kuwa124 <c0mwgave1407@gmail.com>
2024-09-06 17:28:12 +0900

    📱 答えモーダルのフォントをレスポンシブ対応

--- src/components/PositionResultDisplay.tsx
+++ src/components/PositionResultDisplay.tsx
@@ -16,7 +16,7 @@ export const PositionResultDisplay: React.FC<PositionResultDisplayProps> = ({
   }
 
   const Icon = result ? CheckCircle : XCircle;
-  const message: string = result ? '正解！すごい！' : '残念！次は頑張ろう！';
+  const message: string = result ? `正解！すごい！` : '残念！次は頑張ろう！';
   const colorDivClass = result ? 'bg-green-100' : 'bg-red-100';
   const colorIconClass = result ? 'text-green-500' : 'text-red-500';
   const colorPClass = result ? 'text-green-700' : 'text-red-700';
@@ -26,7 +26,7 @@ export const PositionResultDisplay: React.FC<PositionResultDisplayProps> = ({
       className={`fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 text-center p-4 rounded-lg ${colorDivClass}`}
     >
       <div className=' bg-white container mx-auto p-20'>
-        <div className='lg:text-5xl flex justify-center items-center'>
+        <div className='text-sm sm:text-lg md:text-xl lg:text-5xl flex justify-center items-center'>
           <Icon className={`${colorIconClass} mr-2 lg:size-20`} />
           <p className={`${colorPClass} lg:ml-4 font-bold`}>{message}</p>
         </div>
