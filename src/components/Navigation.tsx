2fc32ab6677efe5fa4e0785a642b3dd4ccef3eac
kuwa124e <c0mwgave1407@gmail.com>
2024-09-08 18:30:59 +0900

    💡 ページ番号の取得のコメントを追加

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,7 +6,10 @@ import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const Navigation: React.FC = () => {
+  // 現在のURLパスを取得
   const pathname = usePathname();
+
+  // 'quiz'の後の数字を抽出し、ページ番号として保存（例: '/quiz/3' → 3）。数字がない場合は0。
   const currentPage = parseInt(pathname.split('quiz')[1]) || 0;
 
   return (
