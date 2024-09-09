cd66bda73875a8875c13ee40ffdbf0201f75080a
kuwa124 <c0mwgave1407@gmail.com>
2024-09-09 12:23:49 +0900

    📱 勘定科目の増減問題のmax-wを変更 2xl→md

--- src/app/quiz5/page.tsx
+++ src/app/quiz5/page.tsx
@@ -21,7 +21,7 @@ export default function Quiz5() {
     <main className='text-gray-600 min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500'>
       <Navigation />
       <div className='flex items-center justify-center'>
-        <div className='container max-w-2xl sm:mx-auto bg-white p-2 sm:p-8 m-4 rounded-xl shadow-2xl mt-4 sm:mt-30'>
+        <div className='container max-w-md sm:mx-auto bg-white p-2 sm:p-8 m-4 rounded-xl shadow-2xl mt-4 sm:mt-30'>
           <h1 className='sm:text-3xl text-2xl font-bold text-center mb-6 text-purple-600'>
             勘定科目の増減問題！
           </h1>
