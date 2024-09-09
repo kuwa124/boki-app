ed15dba5bdf3e765d8645cc2cc54cde5a7b3a6fc
kuwa124 <c0mwgave1407@gmail.com>
2024-09-09 12:19:15 +0900

    ⚡️ 問題表示のフォントサイズを大きくし、見やすさを向上

--- src/app/quiz1/components/PositionQuestionDisplay.tsx
+++ src/app/quiz1/components/PositionQuestionDisplay.tsx
@@ -15,8 +15,11 @@ export const PositionQuestionDisplay: React.FC<
       {/* 問題部分 */}
       <div className='bg-yellow-100 p-4 rounded-lg mb-6'>
         <p className='text-lg font-medium text-center'>
-          {question.category}の
-          <span className={`font-bold ${positionColor}`}>
+          <span className='font-bold text-gray-600 text-2xl'>
+            {question.category}
+          </span>
+          の
+          <span className={`font-bold text-2xl ${positionColor}`}>
             {isPosition ? '増加' : '減少'}
           </span>
           は？
--- src/app/quiz5/components/LastQuisDisplay.tsx
+++ src/app/quiz5/components/LastQuisDisplay.tsx
@@ -17,8 +17,11 @@ export const LastQuisDisplay: React.FC<LastQuisDisplayProps> = ({
       {/* 問題部分 */}
       <div className='bg-yellow-100 p-4 rounded-lg mb-6'>
         <p className='text-lg font-medium text-center'>
-          {question.category.text}の
-          <span className={`font-bold ${positionColor}`}>
+          <span className='font-bold text-gray-600 text-2xl'>
+            {question.category.text}
+          </span>
+          の
+          <span className={`font-bold text-2xl ${positionColor}`}>
             {isPosition ? '増加' : '減少'}
           </span>
           は？
