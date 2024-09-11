b1e192890c642e0405626f4fb209d084f9499089
kuwa124 <c0mwgave1407@gmail.com>
2024-09-11 11:47:40 +0900

    🎨 BS科目の追加とコードの整形

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ const PAGE_LIST: page_list[] = [
 export default function Home() {
   return (
     <main className='min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 flex flex-col items-center justify-center'>
-      <div className='bg-white p-8 m-4 rounded-xl shadow-2xl max-w-md  mb-40'>
+      <div className='bg-white p-8 m-4 rounded-xl shadow-2xl max-w-md'>
         <h1 className='sm:text-3xl text-xl font-bold text-center mb-6 text-purple-600 tracking-wider'>
           勘定科目練習問題！
         </h1>
