efd0a1effefc3724d410977a4aa231dc96e24d8b
kuwa124 <c0mwgave1407@gmail.com>
2024-09-24 13:14:06 +0900

    ✨ Quiz4のスコア表示を実装

--- src/app/quiz4/page.tsx
+++ src/app/quiz4/page.tsx
@@ -2,6 +2,7 @@
 
 import { AnswerButtons4 } from '@/app/quiz4/components/AnswerButtons4';
 import { AccountItemQuestionDisplay } from '@/components/AccountItemQuestionDisplay';
+import { GameEndHandler } from '@/components/GameEndHandler';
 import { Loading } from '@/components/Loading';
 import { Navigation } from '@/components/Navigation';
 import { PositionResultDisplay } from '@/components/PositionResultDisplay';
@@ -10,8 +11,16 @@ import { Elements } from '@/constants/type';
 import { useQuiz4 } from '@/hooks/useQuiz4';
 
 export default function Quiz4() {
-  const { question, answer, result, score, totalQuestions, checkAnswer } =
-    useQuiz4();
+  const {
+    question,
+    answer,
+    result,
+    score,
+    totalQuestions,
+    checkAnswer,
+    setScore,
+    setTotalQuestions,
+  } = useQuiz4();
 
   // 回答ボタンがクリックされたときの処理
   const handleAnswer = (position: string) => {
@@ -35,26 +44,35 @@ export default function Quiz4() {
   }
 
   return (
-    <main className='text-gray-600 min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500'>
-      <Navigation />
-      <div className='flex items-center justify-center'>
-        <div className='container sm:mx-auto bg-white p-2 sm:p-8 m-4 rounded-xl shadow-2xl mt-4 sm:mt-30'>
-          <h1 className='sm:text-3xl text-2xl font-bold text-center mb-6 text-purple-600'>
-            総 合 問 題！
-          </h1>
-          <ScoreDisplay score={score} totalQuestions={totalQuestions} />
-          <AccountItemQuestionDisplay question={question} />
-          <AnswerButtons4
-            onAnswer={handleAnswer}
-            isAnswered={answer !== undefined}
-          />
-          <PositionResultDisplay
-            result={result}
-            question={question}
-            position={undefined}
-          />
+    //  GameEndHandlerでゲーム終了処理をラップ
+    <GameEndHandler
+      result={result}
+      score={score}
+      setScore={setScore}
+      setTotalQuestions={setTotalQuestions}
+      totalQuestions={totalQuestions}
+    >
+      <main className='text-gray-600 min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500'>
+        <Navigation />
+        <div className='flex items-center justify-center'>
+          <div className='container sm:mx-auto bg-white p-2 sm:p-8 m-4 rounded-xl shadow-2xl mt-4 sm:mt-30'>
+            <h1 className='sm:text-3xl text-2xl font-bold text-center mb-6 text-purple-600'>
+              総 合 問 題！
+            </h1>
+            <ScoreDisplay score={score} totalQuestions={totalQuestions} />
+            <AccountItemQuestionDisplay question={question} />
+            <AnswerButtons4
+              onAnswer={handleAnswer}
+              isAnswered={answer !== undefined}
+            />
+            <PositionResultDisplay
+              result={result}
+              question={question}
+              position={undefined}
+            />
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </GameEndHandler>
   );
 }
